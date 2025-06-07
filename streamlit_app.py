11dac33d41abf63e4509d7c05d13f1832bfafdc3
man2720 <aymanzabero@gmail.com>
2025-06-07 13:56:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 st.title("Selamat Datang Di Web Informatika")
-st.write(
-    "ngoding seru bersama Belva"
-)
-st.image("IMG_3224.jpeg")
+st.write("ngoding seru bersama Belva")
+st.image("IMG_3224.jpeg", width=500)
 st.title("bjorkababayo")
+
 angka = st.number_input("write a number:", value=0, step=1)
+
 if (angka%2) == 0: 
 st.write (f"{angka} adalah bilangan genap")
 else:
